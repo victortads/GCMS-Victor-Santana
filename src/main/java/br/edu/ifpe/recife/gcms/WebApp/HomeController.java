23aa913910d562c7045aea49acd1c61290bc7e81
victortads <vgs8@discente.ifpe.edu.br>
2024-08-01 14:34:08 -0300

    Adicionando HomeController

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,19 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
+	}
+	
+	@RequestMapping("/victor")
+	public @ResponseBody String greetingVictor() {
+		return "Hello, Víctor";
+	}
+
+}
