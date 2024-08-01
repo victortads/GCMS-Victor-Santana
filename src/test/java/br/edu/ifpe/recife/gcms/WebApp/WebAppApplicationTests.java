57f0ddab5a71d9f0dc803ac8f4e18caef77c18e8
victortads <vgs8@discente.ifpe.edu.br>
2024-08-01 10:36:44 -0300

    1o commit

---
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
