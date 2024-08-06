a6ece08d79444016831aef72f26cbdf6ffc69811
victortads <vgs8@discente.ifpe.edu.br>
2024-08-06 15:53:02 -0300

    Criação e commit da branch hotfix

--- src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -1,31 +1,39 @@
 package br.edu.ifpe.recife.gcms.WebApp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.Matchers.containsString;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class WebAppApplicationTests {
-	@Autowired
-	private MockMvc mockMvc;
 
-	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
-	}
-	
-	@Test
-	public void shouldReturnDefaultMessageVictor() throws Exception {
-		this.mockMvc.perform(get("/victor")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, Víctor")));
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessageVictor() throws Exception {
+        this.mockMvc.perform(get("/victor")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Víctor")));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessageHotfix() throws Exception {
+        this.mockMvc.perform(get("/victor")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, From Hotfix")));
+    }
+
 }
